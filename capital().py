e7b0676160d9953e75ac916078662dfa2d913739
Laladj <77413672+Laladj@users.noreply.github.com>
2021-12-27 12:22:29 +0100

    Create capital().py

---
+++ capital().py
@@ -0,0 +1,14 @@
+#capital d'une banque
+from datetime import date #permet d'avoir l'année, à ignorer
+todays_date = date.today()#permet d'avoir l'année, à ignorer
+capital = float(input("entrez le capital "))
+taeg = float(input("entrez le taux d'interet décimal "))
+annees = int(input("entrew le nombre d'années ou vous souhaitez laisser votre capital "))
+x = 0
+if capital < 0 or taeg < 0:
+	print("erreur")
+elif taeg == 0:
+	x = capital
+else: x=capital*taeg**annees
+
+print(f"vous aurez {x} à la fin de l'année {todays_date.year + annees}")#permet d'avoir l'année, à ignorer 
