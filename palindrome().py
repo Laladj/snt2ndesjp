df6facc2340dda1a192c31e96d6f242581499849
Laladj <77413672+Laladj@users.noreply.github.com>
2022-03-13 20:04:35 +0100

    Update and rename palindrome() to palindrome().py

--- palindrome().py
+++ palindrome().py
@@ -1,7 +1,7 @@
 def palindrome(mot):
     mot = str(mot)  #pour ne pas avoir des soucis de guillemet
     mot = mot.lower()
-    if mot == mot[::-1]:
+    if mot == mot[::-1]: #regarder la documentation en cas de soucis https://docs.python.org/3/library/string.html
         return True
 
 palindrome("kayak") 
