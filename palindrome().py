6fa9203911f99a0c602d886fb76e10a597ae89aa
Laladj <77413672+Laladj@users.noreply.github.com>
2022-03-17 13:22:47 +0100

    Update palindrome().py

--- palindrome().py
+++ palindrome().py
@@ -5,3 +5,15 @@ def palindrome(mot):
         return True
 
 palindrome("kayak") 
+
+def palindrome(mot):
+    liste1, liste2 = [],[]
+    for i in range(0,len(mot)):
+        liste1.append(mot[i])
+    for i in range(1, len(mot)+1):
+        liste2.append(mot[-i])
+    if liste1== liste2: 
+        return True
+    else:
+        return False
+palindrome(kayak)
